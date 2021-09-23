63ca66e458ce165dacdfc324c37bf8ad03cc0125
BO-SEO <boseo@kookmin.ac.kr>
2021-09-23 10:33:28 +0900

    Modify MainCordinator.py

--- MainCordinator.py
+++ MainCordinator.py
@@ -1,73 +1,114 @@
 import serial
 import time
+import requests
 import RPi.GPIO as GPIO
-import re
-GPIO.setmode(GPIO.BCM)
-xbee = serial.Serial()
-xbee.port = "/dev/ttyAMA1"
-xbee.baudrate = 9600
-xbee.timeout = 1
-xbee.writeTimeout = 1
 
-if xbee.is_open == False:
-    xbee.open()
-print("Port open status: ", xbee.is_open)
-print("Receive & Transfer start")
 
-def Receive():
-    xbee.flushInput()
-    rsByte= xbee.readline().strip()
-    print(rsByte)
+class main_cordinator(object):
+    def __init__(self, device_num):
+        # Init Serial COM
+        GPIO.setmode(GPIO.BCM)
+        self.xbee = serial.Serial()
+        self.xbee.port = "/dev/ttyAMA1"
+        self.xbee.baudrate = 9600
+        self.xbee.timeout = 1
+        self.xbee.writeTimeout = 1
 
-    return rsByte
+        # Init device_id
+        self.device_id = device_num
 
-def Transfer(data):
-    if type(data) == type(b''):
-        xbee.write(data)
-    else:
-        data = re.sub('\n', '', data)
-        tsData = bytes(data, 'utf-8')
-        xbee.write(tsData)
-while True:
-    try:
-        print("Please type the device to check (LPG / CO / EXIT) :")
-        tsString = input()
-        if tsString == "LPG":
-            Transfer(tsString)
-            time.sleep(0.1)
-            rsByte = Receive()
-            if rsByte == b'LPGOK':
-                print("LPG status is OK") 
-                #Need to fill below (Communicate with mobile server)
-            elif rsByte == b'LPGWARN':
-                print("LPG WARNING")
-                #Need to fill below (In LPG warning situation, what have to do)
-        
-        elif tsString == "CO":
-            Transfer(tsString)
-            rsByte = Receive()
-            rsString = rsByte.decode('utf-8')
+        # Init requests
+        self.url = 'http://127.0.0.1:52275/data'
+        self.data = {'device_id': device_num, 'status': -1}
+        self.headers = {'Content-Type': 'application/json'}
 
-            if rsString == "WARNING":
-                COstat_Byte = Receive()
-                time.sleep(0.01)
-                COstat = int(COstat_Byte.decode('utf-8'))
-                print("COstat = {0}\n".format(COstat))
-                #Need to fill below (Communicate with mobile server)
+        # Create session
+        self.session = requests.Session()
 
-            else:
-                COstat = int(rsString)
-                print("COstat = {0}\n".format(COstat))
-                #Need to fill below (In CO warning situation, what have to do)
-        elif tsString == "EXIT":
-            break
+        if not self.xbee.is_open:
+            self.xbee.open()
+            print("Port open status: ", xbee.is_open)
+            print("Receive & Transfer start")
+
+    @staticmethod
+    def receive():
+        xbee.flushInput()
+        rsByte = xbee.readline().strip()
+        print(rsByte)
+
+        return rsByte
+
+    @staticmethod
+    def transfer(data):
+        if type(data) == type(b''):
+            xbee.write(data)
         else:
-            print("Please Input the correct command")
-            continue
+            data = re.sub('\n', '', data)
+            tsData = bytes(data, 'utf-8')
+            xbee.write(tsData)
 
-    except KeyboardInterrupt:
-        break
-    time.sleep(0.1)
+    def update_status(self, status, value):
+        self.data = {'device_id': self.device_id, 'status': status, 'value': value}
 
-xbee.close()
-#GPIO.cleanup()
+    def send_data(self, status, value):
+        if not status == '' or not value == '':
+            self.update_status(status, value)
+        try:
+            request_id = self.session.post(self.url, json=self.data, headers=self.headers)
+            if request_id == 200:
+                print("Send success !")
+        except requests.exceptions.RequestException as e:
+            print(e)
+
+    def device_off(self):
+        time.sleep(0.1)
+        self.xbee.close()
+
+
+if __name__ == "__main__":
+    dev = main_cordinator(1)
+    while True:
+        try:
+            print("Please type the device to check (LPG / CO / EXIT) :")
+            tsString = input()
+
+            if tsString == "LPG":
+                dev.transfer(tsString)
+                time.sleep(0.1)
+                rsByte = dev.receive()
+
+                if rsByte == b'LPGOK':
+                    print("LPG status is OK")
+
+                elif rsByte == b'LPGWARN':
+                    print("LPG WARNING")
+
+                # Implements
+                dev.send_data(rsString, '')
+
+            elif tsString == "CO":
+                dev.transfer(tsString)
+                rsByte = dev.receive()
+                rsString = rsByte.decode('utf-8')
+
+                if rsString == "WARNING":
+                    COStat_Byte = dev.receive()
+                    time.sleep(0.01)
+                    COStat = int(COStat_Byte.decode('utf-8'))
+                    print("COstat = {0}\n".format(COStat))
+                else:
+                    COStat = int(rsString)
+                    print("COstat = {0}\n".format(COStat))
+
+                # Implements
+                dev.send_data(rsString, COStat)
+
+            elif tsString == "EXIT":
+                break
+            else:
+                print("Please Input the correct command")
+                continue
+
+        except KeyboardInterrupt:
+            dev.device_off()
+            break
